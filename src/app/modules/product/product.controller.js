b955cf8554707af014f7debcbe59867d266df635
iamraselmolla <raselmolla6336@gmail.com>
2024-10-15 23:48:00 +0600

    added image adding and resixing and uplading

--- src/app/modules/product/product.controller.js
+++ src/app/modules/product/product.controller.js
@@ -2,10 +2,38 @@ import httpStatus from "http-status";
 import catchAsyncFunction from "../../shared/catchAsynFunc.js";
 import sendResponse from "../../shared/sendResponse.js";
 import { ProductServices } from "./product.service.js";
+import sharp from 'sharp';
 
 const createProduct = catchAsyncFunction(async (req, res, next) => {
     try {
-        const product = await ProductServices.createProduct(req.body);
+        const productData = JSON.parse(req.body.data);
+        const mainImages = req.files.images || [];
+        const colorImages = req.files.colors || [];
+
+        const processImage = async (image) => {
+            const filename = `${Date.now()}-${image.originalname.split(' ').join('-')}.png`;
+            const buffer = await sharp(image.buffer)
+                .resize(800, 800)
+                .toFormat('png')
+                .toBuffer();
+            // Save the buffer to your file system or cloud storage here
+            // Example: await fs.promises.writeFile(`./uploads/${filename}`, buffer);
+            return filename; // Return the path or URL to the saved image
+        };
+
+        const processedMainImages = await Promise.all(mainImages.map(processImage));
+        const processedColorImages = await Promise.all(colorImages.map(processImage));
+
+        const newProductData = {
+            ...productData,
+            images: processedMainImages,
+            colors: productData.colors.map((color, index) => ({
+                ...color,
+                image: processedColorImages[index] || null
+            })),
+        };
+
+        const product = await ProductServices.createProduct(newProductData);
         sendResponse(res, {
             statusCode: httpStatus.OK,
             success: true,
@@ -44,21 +72,19 @@ const findProductById = catchAsyncFunction(async (req, res, next) => {
         next(error);
     }
 });
+
 const incrementViewCount = async (req, res, next) => {
     const { id } = req.params;
-
     try {
-        // Find the product by ID and increment the view count
         const product = await ProductServices.incrementViewCount(id);
-
         sendResponse(res, {
             statusCode: httpStatus.OK,
             success: true,
             message: "View count incremented successfully",
             data: product
-        })
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
 
--- src/app/modules/product/product.route.js
+++ src/app/modules/product/product.route.js
@@ -1,13 +1,17 @@
-import express from "express"
+import express from "express";
 import { ProductController } from "./product.controller.js";
+import multer from 'multer';
+
+const upload = multer({ storage: multer.memoryStorage() });
 const ProductRouter = express.Router();
 
-ProductRouter.post("/create-product", ProductController.createProduct);
+ProductRouter.post("/create-product", upload.fields([
+    { name: 'images', maxCount: 5 },
+    { name: 'colors', maxCount: 10 }
+]), ProductController.createProduct);
 
 ProductRouter.get("/get-products", ProductController.getAllProducts);
-
 ProductRouter.get("/get-product/:id", ProductController.findProductById);
-
 ProductRouter.put("/increment-view-count/:id", ProductController.incrementViewCount);
 
 export default ProductRouter;
